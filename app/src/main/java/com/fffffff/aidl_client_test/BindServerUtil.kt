808efe0917e74142829500fe57457f7496914fee
duke <duke@zhihu.com>
2023-09-27 16:39:59 +0800

    init

---
+++ app/src/main/java/com/fffffff/aidl_client_test/BindServerUtil.kt
@@ -0,0 +1,58 @@
+package com.fffffff.aidl_client_test
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+
+/**
+ * @Author: duke
+ * @DateTime: 2023-09-27 16:28:04
+ * @Description: 功能描述：
+ *
+ */
+object BindServerUtil {
+
+    // 数据提供方(服务端)的 Service 包名 + 类名
+    private const val SERVER_SERVICE_CLASS_NAME = "com.fffffff.aidlserver.MyService"
+
+    // 数据提供方(服务端)的 Service 所在的可知行 Module 的包名，比如常见的 App Module
+    // 不是 Service 所在的子 Module 的包名
+    private const val SERVER_SERVICE_PACKAGE_NAME = "com.fffffff.aidl_server_test"
+
+    // 数据提供方(服务端)的 Service 中定义的 Action 值
+    private const val SERVER_SERVICE_ACTION = "com.fffffff.aidlserver.action.MY_SERVICE_CENTER"
+
+    @JvmStatic
+    fun bindService1(activity: Activity?, serviceConnection: ServiceConnection) {
+        val intent = Intent()
+
+        // setComponent() 方式
+        intent.component = ComponentName(SERVER_SERVICE_PACKAGE_NAME, SERVER_SERVICE_CLASS_NAME)
+
+        activity?.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    @JvmStatic
+    fun bindService2(activity: Activity?, serviceConnection: ServiceConnection) {
+        val intent = Intent()
+
+        // setClassName() 方式
+        intent.setClassName(SERVER_SERVICE_PACKAGE_NAME, SERVER_SERVICE_CLASS_NAME)
+
+        activity?.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    @JvmStatic
+    fun bindService3(activity: Activity?, serviceConnection: ServiceConnection) {
+        val intent = Intent()
+
+        // setAction 方式
+        intent.action = SERVER_SERVICE_ACTION
+        intent.setPackage(SERVER_SERVICE_PACKAGE_NAME)
+
+        activity?.bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+}
---
+++ app/src/main/java/com/fffffff/aidl_client_test/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.fffffff.aidl_client_test
+
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.os.Bundle
+import android.os.IBinder
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.fffffff.aidllib.IMyTestAidlInterface
+import com.fffffff.aidllib.IMyTestCallback
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvShow: TextView
+    private var iMyTestAidlInterface: IMyTestAidlInterface? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvShow = findViewById(R.id.tv_show)
+
+        tvShow.setOnClickListener {
+            BindServerUtil.bindService1(this, connection)
+        }
+    }
+
+    private val connection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+
+            tvShow.text = "连接成功"
+
+            iMyTestAidlInterface = IMyTestAidlInterface.Stub.asInterface(service)
+
+            iMyTestAidlInterface?.searchKeyWord(404, "wuhan", object : IMyTestCallback {
+                override fun asBinder(): IBinder? {
+                    return null
+                }
+
+                override fun onResult(p0: String?) {
+                    tvShow.text = p0
+                }
+
+                override fun onFailure(p0: String?) {
+                    tvShow.text = p0
+                }
+
+            })
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            tvShow.text = "连接失败"
+        }
+
+    }
+}
